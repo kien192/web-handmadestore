76c1fe0708c022045a0bbc5fb9938f97bfcb93b3
NTNBackEnd <nguyennghia163tp@gmail.com>
2023-10-20 19:51:53 +0700

    update first main

---
+++ src/main/java/controller/login.java
@@ -0,0 +1,18 @@
+package controller;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+import java.io.*;
+@WebServlet(name = "",value = "")
+public class login extends HttpServlet{
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+    }
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+    }
+
+}
